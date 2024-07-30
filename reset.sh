1bf7bb76722884b2cfe1d281149444a3a58c36e7
Prof. - Xadk3! <xadkeg@duck.com>
2024-07-31 00:28:30 +0545

    .

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kill -9 333; chattr -id /etc/ld.so.preload ; chmod 777 /etc/ld.so.preload ; rm /etc/ld.so.preload ; touch /etc/ld.so.preload; chattr -iad /root/king.txt; chmod 777 /root/king.txt; rm /root/king.txt
+kill -9 333; cd /etc; chattr -id ld.so.preload ; chmod 777 ld.so.preload ; rm ld.so.preload ; touch ld.so.preload; chattr -iad /root/king.txt; chmod 777 /root/king.txt; rm /root/king.txt; cd ->/dev/null
